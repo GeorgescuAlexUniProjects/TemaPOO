1bdbed5eb526c4971e181490870f2023a87aba8c
Alex Georgescu <ionut.georgescu@s.unibuc.ro>
2020-12-06 10:53:11 +0200

    .

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/MasterEnergy/Desktop/Facultate/Programare orientata pe obiecte/Laborator/Tema/Tema_1
+# Install script for directory: C:/Users/MasterEnergy/Desktop/Facultate/Programare orientata pe obiecte/Laborator/Tema/Repo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/MasterEnergy/Desktop/Facultate/Programare orientata pe obiecte/Laborator/Tema/Tema_1/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/MasterEnergy/Desktop/Facultate/Programare orientata pe obiecte/Laborator/Tema/Repo/out/build/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/MasterEnergy/Desktop/Facultate/Programare orientata pe obiecte/Laborator/Tema/Tema_1/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/MasterEnergy/Desktop/Facultate/Programare orientata pe obiecte/Laborator/Tema/Repo/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
