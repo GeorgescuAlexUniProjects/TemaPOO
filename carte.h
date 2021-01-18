c79f2f0d8c1469d70999e0264b57113c0b055f91
Alex Georgescu <ionut.georgescu@s.unibuc.ro>
2021-01-18 23:46:34 +0200

    .

--- carte.h
+++ carte.h
@@ -35,23 +35,6 @@ public:
 	friend std::ostream& operator<<(std::ostream& out, const carte& carte);
 };
 
-class c_fictiune : public carte
-{
-private:
-	std::string nume_carte;
-	autor nume_autor;
-	editura nume_editura;
-	int nr_volume = 0;
-	std::vector<int> nr_pagini_vol = {};
-public:
-	c_fictiune(const std::string& nume_carte, autor nume_autor, editura nume_editura, int nr_volume, std::vector<int> nr_pagini_vol)
-	: nume_carte(nume_carte), nume_autor(nume_autor), nume_editura(nume_editura), nr_volume(nr_volume), nr_pagini_vol(nr_pagini_vol) {}
-	virtual ~c_fictiune() {};
-	int Nr_total_pagini() const;
-	float Medie_pagini() const;
-	void afisare(std::ostream& out) const override;
-};
-
 class carte_factory
 {
 public:
@@ -75,6 +58,23 @@ public:
 	{
 		return carte("paper cover", "non-fictiune", "A4");
 	}
+};
+
+class c_fictiune : public carte
+{
+private:
+	std::string nume_carte;
+	autor nume_autor;
+	editura nume_editura;
+	int nr_volume = 0;
+	std::vector<int> nr_pagini_vol = {};
+public:
+	c_fictiune(const std::string& nume_carte, autor nume_autor, editura nume_editura, int nr_volume, std::vector<int> nr_pagini_vol)
+	: nume_carte(nume_carte), nume_autor(nume_autor), nume_editura(nume_editura), nr_volume(nr_volume), nr_pagini_vol(nr_pagini_vol) {}
+	virtual ~c_fictiune() {};
+	int Nr_total_pagini() const;
+	float Medie_pagini() const;
+	void afisare(std::ostream& out) const override;
 };
 
 class c_n_fictiune : public carte
